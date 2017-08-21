8c4687c476695da3ee3ff130db072b60b37de3aa
blackp <p.blackwell@gmail.com>
2017-08-21 10:11:41 +1000

    Fixup token path

--- generate-github-token.sh
+++ generate-github-token.sh
@@ -3,7 +3,7 @@
 # Generate a new public/private key pair intended for use as a github deploy token
 
 REPO_NAME=$1
-TOKEN_PATH="${HOME}/.ssh/{REPO_NAME}_rsa"
+TOKEN_PATH="${HOME}/.ssh/${REPO_NAME}_rsa"
 PUBLIC_TOKEN_PATH="${TOKEN_PATH}.pub"
 
 [[ -f ${TOKEN_PATH} ]] && echo "private key already exists at ${TOKEN_PATH}" && exit 1
